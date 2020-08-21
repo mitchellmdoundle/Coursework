2c8d73d1ea3467f8fb68fce283c98dacd8c925e8
mitchellmdoundle <57095970+mitchellmdoundle@users.noreply.github.com>
2020-08-21 19:40:23 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,18 @@
+<?php
+session_start();
+$_SESSION = array();
+?>
 <!DOCTYPE html>
 <html>
+<head>
+<title>Login</title>
+</head>
 <body>
-
-<?php
-// test1
-?>
-
+<form action="logincheck.php" method="post">
+  Forename:<input type="text" name="forename"><br>
+  Surname:<input type="text" name="surname"><br>
+  Password:<input type="password" name="passwd"><br>
+  <input type="submit" value="Login">
+</form>
 </body>
-</html> 
+</html>
