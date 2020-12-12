f25af2dba7e8e13c67490c493ba0bd6d25b405d0
mitchellmdoundle <57095970+mitchellmdoundle@users.noreply.github.com>
2020-12-12 17:39:10 +0000

    12/12/20

--- sheet.php
+++ sheet.php
@@ -6,18 +6,22 @@
 <body>
 <?php
 include_once('connection.php');
-$stmt = $conn->prepare("SELECT spells.name as spellname, spells.school as school, spells.spelllevel as spelllevel, spellhastags.tag as tag
+$stmt = $conn->prepare("SELECT spells.name as spellname, spells.school as school, spells.spelllevel as spelllevel,
+ spellhastags.tag as tag
 FROM spellhastags
 INNER JOIN spells
 ON spells.SpellID=spellhastags.SpellID
-WHERE school='Evocation'
+GROUP BY spellname
 ");
 $stmt->execute();
+echo("<table><tr><th>Spell Name</th><th>Spell Level</th><th>Tags</th></tr>");
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
 {
-    print_r($row);
+    echo("<tr><td>".$row['spellname']."</td><td>".$row['spelllevel']."</td><td>".$row['tag']."</td></tr>");
 }
 ?>
+
+
 <br><br><br>
 <a href="http://localhost/coursework/login.php">Log Out</a><br>
 </body>
