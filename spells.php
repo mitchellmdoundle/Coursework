d54034242de16ab0da46c4cf60d6a48a00f9f7b8
mitchellmdoundle <57095970+mitchellmdoundle@users.noreply.github.com>
2020-12-20 20:11:24 +0000

    20/12/2020 part 2
    made the spells sheet actually work and show spells

---
+++ spells.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sheet</title>
+</head>
+<body>
+<table>
+    <tr>
+        <th>Spell Name</th>
+        <th>Spell Level</th>
+        <th>Tags</th>
+    </tr>
+<?php
+include_once('connection.php');
+$stmt = $conn->prepare("SELECT spells.name as spellname, spells.school as school, spells.spelllevel as spelllevel,
+ spellhastags.tag as tag, spells.SpellID as id
+FROM spellhastags
+INNER JOIN spells
+ON spells.SpellID=spellhastags.SpellID
+ORDER BY id
+");
+$stmt->execute();
+
+$uniqueIDs=array();
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+{
+    if (in_array($row['id'], $uniqueIDs))
+    {
+        echo($row['tag']."</td><td>");
+    }
+    else
+    {
+        echo("<tr><td>".$row['spellname']."</td><td>".$row['spelllevel']."</td><td>".$row['tag'])."</td><td>";
+        array_push($uniqueIDs,$row['id']);
+    }
+}
+echo("</td></tr>");
+/*print_r($uniqueIDs);*/
+?>
+</table>
+
+<br><br><br>
+<a href="http://localhost/coursework/login.php">Log Out</a><br>
+</body>
+</html>
